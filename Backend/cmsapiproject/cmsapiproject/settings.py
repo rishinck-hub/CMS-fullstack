1897e37e9e3908794a6abe51a52a3d98c2c7a5a1
Aksasamuval2 <aksasamuel143@gmail.com>
2025-10-23 11:53:44 +0530

    recpasss

--- Backend/cmsapiproject/cmsapiproject/settings.py
+++ Backend/cmsapiproject/cmsapiproject/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cms_backend',
         'USER': 'root',
-        'PASSWORD': 'faith',
+        'PASSWORD': 'aksa123',
         'HOST': 'localhost',
         'PORT': 3306
     }
