21eaf2d0e8fc3ce6ce6db2f8c4a10850b4d247f3
rishinck-hub <rishinkpm@gmail.com>
2026-02-21 14:20:36 +0530

    secret key updated

--- Backend/cmsapiproject/cmsapiproject/settings.py
+++ Backend/cmsapiproject/cmsapiproject/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+th0*hxlg#yc02-@(^$&&jz1_7aw=^t*984xl#ny$&#rwg%!0-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['.onrender.com']
 
